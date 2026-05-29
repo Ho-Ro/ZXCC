33829a01646826f0c9b68418597764579dd541ef
Martin Homuth-Rosemann <Ho-Ro@users.noreply.github.com>
2026-05-29 17:02:21 +0200

    zxcpm that avoids zxcc argument file name translation
    zxcpm calls zxcc but puts a '-' in front of all arguments:
    zxcpm command a bc def -> zxcc command -a -bc -def
    
    Signed-off-by: Martin Homuth-Rosemann <Ho-Ro@users.noreply.github.com>

---
+++ bin/zxcpm.c
@@ -0,0 +1,32 @@
+/* zxcc CP/M emulator program that avoids the zxcc argument translation */
+
+#include "zxcc.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+static char cmdbuf[1024];
+
+
+int main(int argc, char **argv) {
+    int n;
+
+    if ( argc < 2 ) {
+        printf( "usage: zxcpm COMMAND [ ARG ... ]\n");
+        return -1;
+    } else {
+        strcpy( cmdbuf, "zxcc " );
+        strcat( cmdbuf, argv[1] ); // CP/M program name
+        for ( n = 2; n < argc; n++ ) {
+            // prepend all following arguments with '-'
+            // to avoid the zxcc argument parsing
+            // and file name conversion needed for HITECH-C
+            strcat( cmdbuf, " -" );
+	    strcat( cmdbuf, argv[n] );
+	}
+	puts( cmdbuf );
+	return system( cmdbuf );
+    }
+}
